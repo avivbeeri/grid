82e4e185efb65290e56aa9478f131969c2f8469f
Aviv Beeri <aviv.beeri@forgerock.com>
2018-09-13 23:17:30 +0100

    Updated the tilemap to add some better collisions on borders

---
+++ res/tiled/Pavement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Pavement" tilewidth="8" tileheight="8" tilecount="96" columns="8">
+ <image source="../tiles.png" width="64" height="96"/>
+</tileset>
---
+++ res/tiled/Skyscraper.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Skyscraper" tilewidth="8" tileheight="8" tilecount="32" columns="4">
+ <image source="ase/BuildingSketch.png" width="32" height="64"/>
+</tileset>
